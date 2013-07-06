1dde52c4ac855f8c9d1d69e4cfa5948aa42d9caf
MiroshnikovVA <carau@mail.ru>
2013-07-06 10:51:32 +0700

    remove comments 2

--- src/com/suhorukov/miroshnikovva/task3/Main.java
+++ src/com/suhorukov/miroshnikovva/task3/Main.java
@@ -2,10 +2,6 @@ package com.suhorukov.miroshnikovva.task3;
 
 import java.util.Properties;
 
-/**
- * User: MiroshnikovVA
- * Date: 29.06.13
- */
 public class Main {
     public static void main(String[] args) {
         Properties pr =System.getProperties();
