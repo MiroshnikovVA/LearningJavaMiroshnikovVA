672e20083f8618c29495925edc2ab25db4dc3b4c
MiroshnikovVA <carau@mail.ru>
2013-07-13 10:30:21 +0700

    мелкие изменения

--- src/com/suhorukov/miroshnikovva/task3/Main.java
+++ src/com/suhorukov/miroshnikovva/task3/Main.java
@@ -4,16 +4,11 @@ import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //Properties pr =System.getProperties();
-        //for (Object k : pr.keySet())
-        //{
-        //    System.out.println(k + " - " + pr.get(k));
-        //}
-        //System.out.println(pr.get("file.encoding"));
-
-        while (true) {
-            //System.out.println("1");
-            //Thread.sleep(1000);
+        Properties pr =System.getProperties();
+        for (Object k : pr.keySet())
+        {
+            System.out.println(k + " - " + pr.get(k));
         }
+        System.out.println(pr.get("file.encoding"));
     }
 }
